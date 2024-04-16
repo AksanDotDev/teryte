b1218e5931a9dd6bf094a53854810847ba469b05
Alexi Dewfall <git@aksan.dev>
2024-04-16 16:12:43 +0100

    Late out the classes to be for Date and DateSequence

---
+++ teryte/date/date.py
@@ -0,0 +1,54 @@
+import enums
+
+
+class Date:
+    """Basic type for Teryte dates.
+
+    Constructors:
+
+    __new__()
+    _fromeraboundary()
+    fromdatestring()
+    frominignedate()
+    fromordinal()
+
+    Operators:
+    __repr__, __str__
+    __eq__, __le__, __lt__, __ge__, __gt__, __hash__
+    __add__, __radd__, __sub__ (Only with timedelta args)
+
+    Methods:
+
+    todatestring()
+    toinignedate()
+    toordinal()
+    weekday()
+    inigneweekday()
+    month()
+    inignemonth()
+    canonicaldate()
+    equivalentdates()
+    eraordinalday()
+
+    """
+
+
+class DateSequence:
+    """Helper class for formatting sequences of dates.
+
+    Constructors:
+
+    __new__()
+    fromiterstrs()
+    fromiterdates()
+    frominignestrs()
+    fromiterinignedates()
+
+    Methods:
+
+    toliststrs()
+    tolistdates()
+    tolistinignestrs()
+    tolistinignedates()
+
+    """
